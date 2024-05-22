a4dd7e7d956f2b529788993db137a73d597681cb
HaroldMai <149612295+HaroldMai@users.noreply.github.com>
2024-05-23 02:16:43 +0800

    Add files via upload

--- Compare/Client.py
+++ Compare/Client.py
@@ -6,6 +6,13 @@ PORT = 51515
 SERVER = "localhost"
 
 
+uri = "PYRO:honorsCheck@"+SERVER+":"+str(PORT)
+honors_Check=Pyro4.Proxy(uri)
+
+
+person_id = "";
+last_name= "";
+email= "";
 
 def splashScreen():
     print("-------------------------------------")
@@ -17,12 +24,29 @@ def splashScreen():
 
 def checkStudent():
     while True:
-        selection = input("Are you a current OUST Student? (y/n): ").lower()
-        if selection == "y":
-            return True
-        elif selection == "n":
-            return False
+        inValue = getInput("Are you a current OUST Student? (y/n): ").strip().lower()
+        if inValue in ['y', 'n']:
+            if inValue == "y":
+                print("nice")
+                return True
+            elif inValue == "n":
+                return False
+        else:
+            print("Invalid input!!! Choose 'Y' for yes or 'N' for no.")
+            print("")
+            
+def getInput(prompt):
+    editPrompt = f"{prompt}                                     (Enter 'exit' to return to start fo the application)\n"
+    while True:
+        user_input = input(editPrompt).strip()
+        if user_input.lower() == "exit":
+            print("\nReturning to start\n")
+            main()
+            break
+        else:
+            return user_input
 
+    
 def displayScore(grades):
     if grades is None:
         print("Error: No course records found.")
@@ -44,49 +68,82 @@ def displayAverage(course_average):
         print("Error: Could not calculate course average.")
     else:
         print(f"Your course average is: {course_average}")
-def main():
-    uri = "PYRO:honorsCheck@"+SERVER+":"+str(PORT)
-    honors_check=Pyro4.Proxy(uri)
 
-    splashScreen()
-
-    if checkStudent():
+def checkDetail():
+   
+    if checkStudent() == True:
         # If the person is a student
-        person_id = input("Person ID: ").strip()
-        last_name = input("Last Name: ").strip()
-        email = input("OUST Email Address: ").strip()
-
-        if not person_id or not last_name or not email:
-            print("All fields are required.")
-            return
-
-        print("Requesting Data from Server....")
-        try:
-            while True:
-                print("1. Display Scores")
-                print("2. Calculate Course Average")
-                print("3. Exit")
-                choice = input("Enter your choice: ").strip()
-
-                if choice == "1":
-                    scores = honors_check.displayScore(person_id)
-                    print("Requesting returns from Server....")
-                    displayScore(scores)
-                elif choice == "2":
-                    course_average = honors_check.calculateCourseAverage()
-                    print("Requesting course average from Server....")
-                    displayAverage(course_average)
-                elif choice == "3":
-                    print("Exiting...")
-                    break
+        while True: 
+            person_id = getInput("What is your Student ID? ").strip()
+            if person_id:
+                if not person_id.isdigit():
+                    print("\nEnter digits for Student ID\n")
                 else:
-                    print("Invalid choice. Please try again.")
-        except Pyro4.errors.PyroError as e:
-            print(f"Failed to retrieve data from the server: {e}")
-    else:
-        # If the person is not a student
-        print("You cannot use this system - Bye")
+                    break
+            else:
+                print("\n Student ID cannot be empty!!!\n")
 
-    print("")
+        while True: 
+            last_name = getInput("What is your Last Name? ").strip()
+            if last_name:
+                break
+            else:
+                print("\nLast Name cannot be empty!!!\n")
+                      
+        while True: 
+            email = getInput("What is you OUST Email Address? ").strip()
+            if email:
+                break
+            else:
+                print("\nEmail cannot be empty!!!\n")
+                
+        print("Requesting Check Student Data from Client server....")
+         # If the person is not a student
+        #if honors_Check.checkStudent(person_id, last_name, email) == False:
+            #main()
+        menu()
+    else:
+         print("Manually input your scores")
+
+
+def menu(): 
+    try:
+        while True:
+            print("1. Display Scores")
+            print("2. Calculate Course Average")
+            print("3. Calculate 8 best scores")
+            print("4. Exit")
+            choice = input("Enter your choice: ").strip()
+
+            if choice == "1":
+                scores = honors_Check.displayScore(person_id)
+                print("Requesting returns from Server....")
+                displayScore(scores)
+            elif choice == "2":
+                course_average = honors_Check.honoursEvaluation()
+                print("Requesting course average from Server....")
+                displayAverage(course_average)
+            elif choice == "3":
+                best_8_average = honors_Check.calculateBest8Average(person_id)
+                print("Requesting average of best 8 scores from Server....")
+                displayAverage(best_8_average)
+            elif choice == "4":
+                print("Exiting...")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+    except Pyro4.errors.PyroError as e:
+        print("Failed to retrieve data from the server: {e}")
+        print("")
+
+
+
+    
+def main():
+    splashScreen()
+    checkDetail()
+    honors_Check.honoursEvaluation()
+    
 if __name__ == "__main__":
     main()
+
